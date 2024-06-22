765f0751ad3899685dfe158b9d11c3061d3135c9
Asina07 <85829716+Asina07@users.noreply.github.com>
2024-06-22 16:12:09 +0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,6 @@ app.use(express.json()); // allow json data in the req body
 app.use(
   cors({
     origin: ["https://my-duct-frontend.vercel.app"],
-    method: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
 ); // Use the cors middleware
